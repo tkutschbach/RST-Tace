70f39e8e1b09772e3d91aa4beeac0cd86d06ba87
Tino Kutschbach <tino.kutschbach@mailbox.org>
2019-05-25 17:58:58 +0200

    removing dead code + changes in CLI-help texts

--- rsttace/commandline.py
+++ rsttace/commandline.py
@@ -7,7 +7,6 @@ Created on Sat Jul 14 14:40:01 2018
 
 import click
 
-from rsttace.controller import Interactor
 from rsttace.controller.interactors import AnalyseInteractor, CompareInteractor
 from rsttace.input import RstTreeParser
 from rsttace.output import RelTableLogger, RelTableCliOutput
@@ -20,20 +19,20 @@ def cli():
     pass
 
 
-@cli.command('analyse', short_help="Parse single RST trees, analyse and list\
-                                    their annotated relations")
+@cli.command('analyse', short_help="Parse a single RST tree, analyse and list\
+                                    its annotated relations.")
 @click.argument("RSTFILE")
 @click.option("--outputfile", "-o",
               default="",
               metavar="FILEPATH",
-              help="Write the result list to FILEPATH (as CSV)")
+              help="Write the result list of annotations to FILEPATH (as CSV)")
 @click.option('--verbose', '-v', is_flag=True,
               help="Print results on command line")
 def analyse(rstfile: str,
             outputfile: str,
             verbose: bool):
-    """ Parse the RST-tree from RSTFILE, and create a list of the rethorical
-    relations annotated inside it. """
+    """ Parse the RST-tree from RSTFILE, and create a list of the rethorical \
+relations annotated inside it. """
     rstParser = RstTreeParser(rstfile)
 
     tableOutputs = []
@@ -47,14 +46,14 @@ def analyse(rstfile: str,
     interactor.run()
 
 
-@cli.command('compare', short_help="Parse RST tree pairs and \
-                                    compare them with each other")
+@cli.command('compare', short_help="Parse an RST tree pair and compare\
+                                    both trees with each other.")
 @click.argument("RSTFILE1")
 @click.argument("RSTFILE2")
 @click.option("--outputfile", "-o",
               default="",
               metavar="FILEPATH",
-              help="Write the result list to FILEPATH (as CSV)")
+              help="Write the resulting comparison table to FILEPATH (as CSV)")
 @click.option('--verbose', '-v', is_flag=True,
               help="Print results on command line")
 def compare(rstfile1: str,
@@ -62,7 +61,7 @@ def compare(rstfile1: str,
             outputfile: str,
             verbose: bool):
     """ Parse two RST-trees, from RSTFILE1 and RSTFILE2 respectively, \
-    compare their annotated relations, and create a comparison table. """
+compare their annotated relations, and create a comparison table. """
     rstParser1 = RstTreeParser(rstfile1)
     rstParser2 = RstTreeParser(rstfile2)
 
@@ -81,15 +80,25 @@ def compare(rstfile1: str,
 
 @cli.command('evaluate', short_help="Perform a statistical evaluation\
                                      of a set of RST tree pairs.")
-@click.argument("RSTFILE1")
-@click.argument("RSTFILE2")
+@click.argument("DIRECTORY1")
+@click.argument("DIRECTORY2")
 @click.option("--outputfile", "-o",
               default="",
               metavar="FILEPATH",
-              help="Write the result list to FILEPATH (as CSV)")
-def evaluate(rstfile1: str,
-             rstfile2: str,
-             outputfile: str):
+              help="Write the result table to FILEPATH (as CSV)")
+@click.option('--verbose', '-v', is_flag=True,
+              help="Print results on command line")
+def evaluate(directory1: str,
+             directory2: str,
+             outputfile: str,
+             verbose: bool):
+    """ Parse and compare two sets of RST-trees (in DIRECTORY1 and DIRECTORY2 \
+respectively) with each other and calculate statistical metrics. \
+In order to associate RST-trees of both sets with each other, the files \
+belonging to each other must have the same name (i.e., for each *.rs3 file in \
+DIRECTORY1, the file with the equivalent name is read from DIRECTORY2 and used\
+ for comparison with the first one). """
+
     print("Sorry, this functionality is not implemented yet.")
     pass
 
--- rsttace/controller/__init__.py
+++ rsttace/controller/__init__.py
@@ -27,38 +27,3 @@ class IComparisonTableOutput(ABC):
     @abstractmethod
     def write(self, compTable: ComparisonTable):
         pass
-
-
-# Use-Case Interactor definition #############################################
-
-
-class Interactor():
-    tableGenerator: TableGenerator
-    tableEvaluator: TableEvaluator
-
-    def __init__(self,
-                 rstIn1: IRstInput,
-                 rstIn2: IRstInput,
-                 relTableOut1: IRelTableOutput,
-                 relTableOut2: IRelTableOutput,
-                 compTableOut: IComparisonTableOutput):
-
-        self.tableGenerator = TableGenerator()
-        self.tableEvaluator = TableEvaluator()
-
-        self.rstReader1 = rstIn1
-        self.rstReader2 = rstIn2
-        self.relTableWriter1 = relTableOut1
-        self.relTableWriter2 = relTableOut2
-        self.compTableWriter = compTableOut
-
-        return
-
-    def run(self):
-        relTable1 = self.tableGenerator.run(self.rstReader1.read())
-        relTable2 = self.tableGenerator.run(self.rstReader2.read())
-        self.relTableWriter1.write(relTable1)
-        self.relTableWriter2.write(relTable2)
-        compTable = self.tableEvaluator.run(relTable1, relTable2)
-        self.compTableWriter.write(compTable)
-        return
